520e03bb3a4631266cb3b80e48277c22fc024c84
philisiah <sphilisiah@gmail.com>
2018-10-11 14:50:00 +0000

    Update requirements

--- codelnmain/settings.py
+++ codelnmain/settings.py
@@ -101,12 +101,8 @@ WSGI_APPLICATION = 'codelnmain.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'codeln_main',
-        'USER': 'postgres',
-        'PASSWORD': 'Password007',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
