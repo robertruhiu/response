c5451957bb0ad4240fe68e60eb096a311353be16
philisiah <sphilisiah@gmail.com>
2018-11-05 10:53:07 +0000

    update:settings

--- codelnmain/settings.py
+++ codelnmain/settings.py
@@ -158,7 +158,12 @@ django_heroku.settings(locals())
 
 # allauth settings
 ACCOUNT_AUTHENTICATED_LOGIN_REDIRECTS = True
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'philisiah@codeln.com'
+EMAIL_HOST_PASSWORD = 'Codeln_2018#'
 ACCOUNT_AUTHENTICATION_METHOD = ('email')
 
 ACCOUNT_LOGIN_ON_EMAIL_CONFIRMATION = True
