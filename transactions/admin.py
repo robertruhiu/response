ac5c89b88d1c8403237c4cbe41ab4000f1daf792
denni <robertruhiu@gmail.com>
2018-12-08 16:11:41 +0000

    admin board to show more

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from transactions.models import Candidate, Transaction
-
+from classroom.models import Quiz,Student,TakenQuiz,Question,Answer,StudentAnswer,Subject
 from frontend.models import candidatesprojects
 
 # Register your models here.
@@ -24,3 +24,10 @@ class TransactionAdmin(admin.ModelAdmin):
 
 admin.site.register(Transaction, TransactionAdmin)
 admin.site.register(candidatesprojects)
+admin.site.register(Quiz)
+admin.site.register(Subject)
+admin.site.register(Student)
+admin.site.register(StudentAnswer)
+admin.site.register(TakenQuiz)
+admin.site.register(Question)
+admin.site.register(Answer)
