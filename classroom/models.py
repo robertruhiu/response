df1981afdc1274ea6442adb01d4e8718c5b75354
denni <robertruhiu@gmail.com>
2018-12-18 18:02:09 +0000

    migrations take two

--- classroom/models.py
+++ classroom/models.py
@@ -11,6 +11,7 @@ class Subject(models.Model):
         return self.name
 
 
+
 class Quiz(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='quizzes')
     name = models.CharField(max_length=255)
