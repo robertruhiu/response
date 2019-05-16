a94fe32ed51848d981673545724be240ff6132de
frenx <robertruhiu@gmail.com>
2019-05-16 14:08:10 +0000

    debug true

--- codelnmain/settings.py
+++ codelnmain/settings.py
@@ -26,7 +26,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = config('SECRET_KEY', default='SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ENVIRONMENT = config('ENVIRONMENT', default='local')
 
